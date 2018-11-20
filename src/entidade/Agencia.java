34ce69fc0cd584260091c881027b25f39305cae3
Unknown <arthurdourado01@hotmail.com>
2018-11-19 22:00:59 -0200

    Criando Classe de Negócio
    Definindo esqueleto da classe negócio

--- src/entidade/Agencia.java
+++ src/entidade/Agencia.java
@@ -3,4 +3,63 @@ package entidade;
 
 public class Agencia {
     
+    private int identificador;
+    private String nome;
+    private int cnpj;
+    private String endereco;
+    private int telefone;
+
+    public Agencia(int identificador, String nome, int cnpj, String endereco, int telefone) {
+        this.identificador = identificador;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Agencia() {
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+    
 }
---
+++ src/negocio/NAgencia.java
@@ -0,0 +1,55 @@
+package negocio;
+
+import entidade.Agencia;
+import java.sql.SQLException;
+import java.util.List;
+import persistencia.PAgencia;
+
+public class NAgencia {
+    
+    PAgencia persistencia;
+
+    public NAgencia() {
+        this.persistencia = new PAgencia();
+    }
+
+    public void salvar(Agencia parametro) throws SQLException, Exception {
+
+        if (parametro.getNome().isEmpty()) {
+            throw new Exception("É necessário informar o nome da agência!");
+        }
+        if (parametro.getCnpj() == 0) {
+            throw new Exception("É necessário informar o CNPJ da agência!");
+        }
+        if (parametro.getEndereco().isEmpty()) {
+            throw new Exception("É necessário informar o endereço da agência!");
+        }
+        if (parametro.getTelefone() == 0) {
+            throw new Exception("É necessário informar o telefone da agência!");
+        }
+        
+        
+        if (parametro.getIdentificador()== 0) {
+            persistencia.incluir(parametro);
+        } else {
+            persistencia.alterar(parametro);
+        }
+        
+    }
+
+    public void excluir(int parametro) throws SQLException {
+        persistencia.excluir(parametro);
+    }
+
+    public Agencia consultar(int parametro) throws SQLException {
+        return persistencia.consultar(parametro);
+    }
+
+    public List<Agencia> listar() throws SQLException {
+        return persistencia.listar();
+    }
+}
+
+    
+    
+
